ff134f63cb16844c0421c75bc1eda4da8cd582bd
JhonnatanRodriguez <jhonn@outlook.com>
2014-09-06 02:34:49 -0400

    Refactoring +  Fixed Bugs

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 
-	var risePay = require('../lib/risepay');
+	var risePay = require('risepay');
 
 	risePay.connect({
 		username: "demo",
@@ -21,10 +21,15 @@ module.exports = function(app){
 									Zip: "33139",
 									Street: "1880 Alton Road",
 									Customer: "JR"
-									}, function(data){
+									}, function(resp){
 											
-
-						  					res.send(data.json);
+											if(resp.RespMSG == "Approved"){
+												console.log("Approved. Transaction ID = " + resp.PNRef);
+												console.log("AuthCode = " + resp.AuthCode)
+											}else{
+												console.log("Declined: " + resp.Message);
+											}
+						  					res.send(resp);
 									});	
 
 								});
